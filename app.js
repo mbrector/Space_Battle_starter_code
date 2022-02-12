d5d0467ae3bd08cb1a9ecf6a2d5d0e4125e339c6
unknown <mbrector@gmail.com>
2022-02-11 20:56:29 -0500

    MVP

--- app.js
+++ app.js
@@ -44,57 +44,75 @@ class Enemy extends Ships{
         this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10  
 }
 attack(opp)
-    {
-        let random = Math.random();
-
-        if (this.accuracy > random)
-        {
-            alert('They hit ' + this.firepower + ' on our ' + opp.hull + ' hull!')
-            console.log('They hit ' + this.firepower + ' on our ' + opp.hull + ' hull!')
-            opp.hull -= this.firepower;
-            if (opp.hull <= 0) {
-                opp.alive = false;
-                alert('We have perished.')
-                console.log('We have perished.');
-            }
-        } 
-        else 
-        {
-            alert('They missed!')
-            console.log('They missed!')
-        }
-        alert('We have ' + opp.hull + ' hull left!')
-        console.log('We have ' + opp.hull + ' hull left!');
-    }
-}
-
-function battle(we, enemy)
 {
-    for(let i = 0; i < enemy.length; i++)
+    let random = Math.random();
+    
+    if (this.accuracy > random)
     {
-        if(we.alive){
-            alert("\n\nBattle " + (i+1))      
-            console.log("\n\nBattle " + (i+1))
+        alert('They hit ' + this.firepower + ' on our ' + opp.hull + ' hull!')
+        console.log('They hit ' + this.firepower + ' on our ' + opp.hull + ' hull!')
+        opp.hull -= this.firepower;
+        if (opp.hull <= 0) {
+            opp.alive = false;
+            alert('We have perished.')
+            console.log('We have perished.');
         }
-        while (we.alive && enemy[i].alive)
-        {  
-            we.attack(enemy[i]);
-            if (enemy[i].alive == true)
-            {
-                enemy[i].attack(we);
-            }
-        }
+    } 
+    else 
+    {
+        alert('They missed!')
+        console.log('They missed!')
     }
+    alert('We have ' + opp.hull + ' hull left!')
+    console.log('We have ' + opp.hull + ' hull left!');
+}
 }
 
 let killerB = new USSSchwarzenegger(20, 5, 0.7)
 
 let enemies = [];
-
 for (let i = 0; i < 6; i++)
 {
     enemies[i] = new Enemy();
 }
+
+function continueBattle (){
+    let cont = prompt("Should we attack the enemy?", "Yes or No")
+        if(cont.toLowerCase() === "yes"){
+          return true
+     
+      }else{
+          
+          (cont.toLowerCase()==='no')
+              alert("Mission failed.")
+              return false
+  
+      }
+  }
+
+function battle(we, enemy){
+    for(let i = 0; i < enemy.length; i++){
+        document.querySelector("#enemyNums").innerHTML = `Hull : ${enemy[i].hull} <br> FirePower : ${enemy[i].firepower} <br> Accuracy : ${enemy[i].accuracy}`
+        document.querySelector("#playerNums").innerHTML = `Hull : ${we.hull} <br> FirePower : ${we.firepower} <br> Accuracy : ${we.accuracy}`
+        if(we.alive){
+            alert("\n\nBattle " + (i+1))      
+            console.log("\n\nBattle " + (i+1))
+        }
+        if(continueBattle()=== true){
+        while (we.alive && enemy[i].alive){  
+            we.attack(enemy[i]);
+            document.querySelector("#enemyNums").innerHTML = `Hull : ${enemy[i].hull} <br> FirePower : ${enemy[i].firepower} <br> Accuracy : ${enemy[i].accuracy}`
+            if (enemy[i].alive == true)
+            {
+                enemy[i].attack(we);
+                document.querySelector("#playerNums").innerHTML = `Hull : ${we.hull} <br> FirePower : ${we.firepower} <br> Accuracy : ${we.accuracy}`
+            }
+        }
+    }
+    else{break}
+    }
+}
+
 
 console.log(enemies)
 
@@ -102,4 +120,3 @@ console.log(enemies)
 
 battle(killerB, enemies)
 
-let start = prompt('Can you defend your universe?', 'Yes or No')
